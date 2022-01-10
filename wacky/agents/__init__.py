f897303215872dbb61131d910a12f70a6cfd0d97
maik97 <35041716+maik97@users.noreply.github.com>
2022-01-10 23:37:04 +0100

    update

--- wacky/agents/__init__.py
+++ wacky/agents/__init__.py
@@ -1,2 +1,3 @@
 from wacky.agents.actor_critic_architectures import *
 from wacky.agents.base_learner import *
+from wacky.agents.pre_built import *
--- wacky/agents/pre_built/__init__.py
+++ wacky/agents/pre_built/__init__.py
@@ -1,1 +1,2 @@
-
+from wacky.agents.pre_built.reinforce import *
+from wacky.agents.pre_built.advantage_actor_critic import *
