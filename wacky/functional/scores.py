a03f456ddc1d35f2dfa8b8b755536f12cb562bad
maik97 <35041716+maik97@users.noreply.github.com>
2022-01-26 21:56:38 +0100

    update

--- wacky/functional/scores.py
+++ wacky/functional/scores.py
@@ -18,12 +18,27 @@ def monte_carlo_returns(rewards, gamma=0.99, eps=1e-07, standardize=False):
     return th.unsqueeze(returns, dim=-1)
 
 
+def temporal_difference_returns(rewards, dones, values, next_values, gamma=0.99, eps=1e-07, standardize=False):
+
+    returns = []
+    for i in range(len(rewards)):
+        returns.append(rewards[i] + gamma * next_values[i] * (1-int(dones[i])) - values[i])
+
+    returns = th.tensor(returns)
+
+    if standardize:
+        returns = funky.standardize_tensor(returns, eps)
+
+    return th.unsqueeze(returns, dim=-1)
+
+
 def generalized_returns(rewards, dones, values, next_values, gamma=0.99, lamda=0.95, eps=1e-07, standardize=False):
+
     g = 0.0
     returns = []
     for i in reversed(range(len(rewards))):
         delta = rewards[i] + gamma * next_values[i] * dones[i] - values[i]
-        g = delta + gamma * lamda * int(dones[i]) * g
+        g = delta + gamma * lamda * (1-int(dones[i])) * g
         future_return = g + values[i]
         returns.insert(0, future_return)
 
@@ -35,7 +50,30 @@ def generalized_returns(rewards, dones, values, next_values, gamma=0.99, lamda=0
     return th.unsqueeze(returns, dim=-1)
 
 
+def n_step_returns(rewards, dones, values, next_values, n=16, gamma=0.99, lamda=1.0, eps=1e-07, standardize=False):
+
+    returns = []
+
+    for i in range(len(rewards)):
+        g = 0.0
+        future_rewards = rewards[i:i+n] if len(rewards) > i+n else rewards[i:]
+
+        for j in reversed(range(len(future_rewards))):
+            delta = future_rewards[j] + gamma * next_values[i+j] * dones[i+j] - values[i+j]
+            g = delta + gamma * lamda * (1-int(dones[i+j])) * g
+
+        returns.append(g + values[i])
+
+    returns = th.tensor(returns)
+
+    if standardize:
+        returns = funky.standardize_tensor(returns, eps)
+
+    return th.unsqueeze(returns, dim=-1)
+
+
 def calc_advantages(returns, values, eps=1e-07, standardize=False):
+
     advantages = th.sub(returns, values)
     if standardize:
         advantages = funky.standardize_tensor(advantages, eps)
@@ -51,4 +89,3 @@ def generalized_advantage_estimation(
     advantages = calc_advantages(returns, values, eps, standardize_advantages)
 
     return returns, advantages
-
--- wacky/scores/return_scores.py
+++ wacky/scores/return_scores.py
@@ -1,7 +1,7 @@
-import torch as th
-import numpy as np
+import abc
 from wacky import functional as funky
 from wacky.memory.running_mean_std import RunningMeanStd
+
 
 class BaseReturnCalculator(funky.MemoryBasedFunctional):
 
@@ -9,12 +9,14 @@ class BaseReturnCalculator(funky.MemoryBasedFunctional):
             self,
             reward_calc_rms=False,
             reward_eps_rms=1e-4,
-            reward_substact_mean=False,
+            reward_norm_eps=1e-8,
+            reward_subtract_mean=False,
             reward_min=None,
             reward_max=None,
             return_calc_rms=False,
             return_eps_rms=1e-4,
-            return_substact_mean=False,
+            return_norm_eps=1e-8,
+            return_subtract_mean=False,
             return_min=None,
             return_max=None,
     ):
@@ -22,25 +24,26 @@ class BaseReturnCalculator(funky.MemoryBasedFunctional):
 
         self.reward_calc_rms = reward_calc_rms
         if self.reward_calc_rms:
-            self.rms_reward = RunningMeanStd(reward_eps_rms, shape=())
-            self.reward_substact_mean = reward_substact_mean
+            self.rms_reward = RunningMeanStd(reward_eps_rms, shape=(), norm_eps=reward_norm_eps)
+            self.reward_subtract_mean = reward_subtract_mean
             self.reward_min = reward_min
             self.reward_max = reward_max
 
         self.return_calc_rms = return_calc_rms
         if self.reward_calc_rms:
-            self.rms_return = RunningMeanStd(return_eps_rms, shape=())
-            self.return_substact_mean = return_substact_mean
+            self.rms_return = RunningMeanStd(return_eps_rms, shape=(), norm_eps=return_norm_eps)
+            self.return_subtract_mean = return_subtract_mean
             self.return_min = return_min
             self.return_max = return_max
 
+    @abc.abstractmethod
     def call(self, memory):
-        pass
+        raise NotImplementedError()
 
     def rms_normalize_rewards(self, rewards):
         if self.reward_calc_rms:
             return self.rms_reward.normalize(
-                rewards, True, self.reward_substact_mean, self.reward_min, self.reward_max
+                rewards, True, self.reward_subtract_mean, self.reward_min, self.reward_max
             )
         else:
             return rewards
@@ -48,28 +51,52 @@ class BaseReturnCalculator(funky.MemoryBasedFunctional):
     def rms_normalize_returns(self, returns):
         if self.return_calc_rms:
             return self.rms_return.normalize(
-                returns, True, self.return_substact_mean, self.return_min, self.return_max
+                returns, True, self.return_subtract_mean, self.return_min, self.return_max
             )
         else:
             return returns
 
 
-class MonteCarloReturns(funky.MemoryBasedFunctional):
+class MonteCarloReturns(BaseReturnCalculator):
 
-    def __init__(self, gamma=0.99, eps=1e-07, standardize=False):
-        super().__init__()
+    def __init__(self, gamma=0.99, eps=1e-07, standardize=False, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
         self.gamma = gamma
         self.eps = eps
         self.standardize = standardize
 
     def call(self, memory):
-        return funky.monte_carlo_returns(
-            rewards=memory['rewards'],
+        returns = funky.monte_carlo_returns(
+            rewards=self.rms_normalize_rewards(memory['rewards']),
             gamma=self.gamma,
             eps=self.eps,
             standardize=self.standardize,
         )
+        return self.rms_normalize_returns(returns)
+
+
+class GeneralizedReturns(BaseReturnCalculator):
+
+    def __init__(self, gamma=0.99, lamda=0.95, eps=1e-07, standardize=False, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.gamma = gamma
+        self.lamda = lamda
+        self.eps = eps
+        self.standardize = standardize
+
+    def call(self, memory):
+        returns = funky.generalized_returns(
+            rewards=self.rms_normalize_rewards(memory['rewards']),
+            dones=memory['dones'],
+            values=memory['values'],
+            next_values=memory['next_values'],
+            gamma=self.gamma,
+            lamda=self.lamda,
+            eps=self.eps,
+            standardize=self.standardize)
+        return self.rms_normalize_returns(returns)
 
 
 class CalcAdvantages(funky.MemoryBasedFunctional):
@@ -87,3 +114,19 @@ class CalcAdvantages(funky.MemoryBasedFunctional):
             eps=self.eps,
             standardize=self.standardize,
         )
+
+
+class GeneralizedAdvantageEstimation(funky.MemoryBasedFunctional):
+
+    def __init__(self, gamma=0.99, lamda=0.95, eps=1e-07,
+                 standardize_returns=False, standardize_advantages=False, *args, **kwargs):
+        super().__init__()
+
+        self.returns_calculator = GeneralizedReturns(gamma, lamda, eps, standardize_returns, *args, **kwargs)
+        self.advantage_calculator = CalcAdvantages(eps, standardize_advantages)
+
+    def call(self, memory):
+        returns = self.returns_calculator(memory)
+        memory['returns'] = returns
+        advantages = self.advantage_calculator(memory)
+        return returns, advantages
