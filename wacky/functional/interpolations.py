642f4678c68b8a5845d5749161a2970e0145342f
maik97 <35041716+maik97@users.noreply.github.com>
2022-01-27 07:14:16 +0100

    update

--- wacky/functional/interpolations.py
+++ wacky/functional/interpolations.py
@@ -1,8 +1,34 @@
 import numpy as np
 from scipy import interpolate
+from wacky import functional as funky
 
 
-def get_ramp_interpolator(
+def check_list(val):
+    if not isinstance(val, list):
+        val = [val]
+    return val
+
+
+def assign_ramp_points(val_a: float, val_b: float, point_a: float, point_b: float):
+    x_vals = np.array([])
+    y_vals = np.array([])
+    val_a, val_b, point_a, point_b = check_list(val_a), check_list(val_b), check_list(point_a), check_list(point_b)
+    if point_a[0] == 0.0:
+        x_vals = np.append(x_vals, point_a)
+        y_vals = np.append(y_vals, val_a)
+    else:
+        x_vals = np.append(x_vals, [0.0, *point_a])
+        y_vals = np.append(y_vals, [val_a[0], *val_a])
+    if point_b[-1] == 1.0:
+        x_vals = np.append(x_vals, point_b)
+        y_vals = np.append(y_vals, val_b)
+    else:
+        x_vals = np.append(x_vals, [*point_b, 1.0])
+        y_vals = np.append(y_vals, [*val_b, val_b[-1]])
+    return x_vals, y_vals
+
+
+def get_ramp_interp1d(
         val_a: float,
         val_b: float,
         point_a: float,
@@ -35,9 +61,71 @@ def get_ramp_interpolator(
     :return:
         interpolate.interp1d object, that interpolates the ramp, when called
     """
+    x, y = assign_ramp_points(val_a, val_b, point_a, point_b)
     return interpolate.interp1d(
         x=np.array([0.0, point_a, point_b, 1.0]),
         y=np.array([val_a,val_a, val_b, val_b]),
         kind=kind,
         *args, **kwargs
     )
+
+class RampInterpolator(funky.WackyBase):
+
+    def __init__(
+            self,
+            val_a: [float, list],
+            val_b: [float, list],
+            point_a: [float, list],
+            point_b: [float, list],
+            kind: str = 'linear',
+            clip=True,
+            *args, **kwargs
+    ):
+        super(RampInterpolator, self).__init__()
+
+        self.x_vals, self.y_vals= assign_ramp_points(val_a, val_b, point_a, point_b)
+        print(self.x_vals)
+        print(self.y_vals)
+
+        self.f = interpolate.interp1d(
+            x=self.x_vals,
+            y=self.y_vals,
+            kind=kind,
+            *args, **kwargs
+        )
+        self.clip = clip
+        if self.clip:
+            self.a_min = np.min(self.y_vals)
+            self.a_max = np.max(self.y_vals)
+
+    def call(self, x):
+        y = self.f(x)
+        if self.clip:
+            y = np.clip(y, self.a_min, self.a_max)
+        return y
+
+    def plot(self, x_new=None, mode='show'):
+        import matplotlib.pyplot as plt
+        if x_new is None:
+            x_new = np.arange(0, 1.01, 0.01)
+        y_new = self(x_new)
+        plt.plot(self.x_vals, self.y_vals, 'o', x_new, y_new, '-')
+        if mode == 'show':
+            plt.show()
+
+
+def main():
+
+    ramp_interp = RampInterpolator(
+        val_a=[1.0],
+        val_b=[0.55, 0.5, 0.45,0.01],
+        point_a=[0.0],
+        point_b=[0.4, 0.5, 0.6,0.9],
+        kind='quadratic'
+    )
+
+    ramp_interp.plot()
+
+
+if __name__ == '__main__':
+    main()
